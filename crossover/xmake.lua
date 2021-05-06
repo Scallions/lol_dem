641096b0bc86a46137f7d02a4118040951471d80
Scallions <844120806@qq.com>
2021-05-06 16:40:25 +0800

    change vcpkg to xrepo

--- crossover/xmake.lua
+++ crossover/xmake.lua
@@ -1,10 +1,13 @@
 add_rules("mode.debug", "mode.release")
 
+add_requires("boost")
+
 target("crossover")
     set_kind("binary")
     add_files("src/main.cpp")
+    add_packages("boost")
     on_load(function (target)
-        target:add(find_packages("vcpkg::boost-filesystem"))
+        target:add(find_packages("boost"))
     end)
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
--- read_data/xmake.lua
+++ read_data/xmake.lua
@@ -1,10 +1,13 @@
 add_rules("mode.debug", "mode.release")
 
+add_requires("boost")
+
 target("read_data")
     set_kind("binary")
     add_files("src/*.cpp")
+    add_packages("boost")
     on_load(function (target)
-        target:add(find_packages("vcpkg::boost-filesystem"))
+        target:add(find_packages("boost"))
     end)
 
 --
