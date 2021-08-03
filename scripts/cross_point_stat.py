8b97d7ba59f805c36be148730407f49d0bbf6cb2
Scallions <844120806@qq.com>
2021-08-03 20:10:15 +0800

    cache

--- scripts/cross_point_stat.py
+++ scripts/cross_point_stat.py
@@ -3,6 +3,7 @@
 import os
 import pandas as pd 
 from constant import *
+import matplotlib.pyplot as plt
 
 
 cp_fp = "crossoverO.txt" # 交叉点文件信息
@@ -13,9 +14,17 @@ df = pd.read_csv(os.path.join(DIR, cp_fp), names=["aorbit", "dorbit", "aidx", "d
 
 
 # 按轨道进行统计
-print(df[["aorbit", "dalt"]].groupby("aorbit").mean().abs().sort_values(by="dalt", ascending=True).head())
+acps = df[["aorbit", "dalt"]].groupby("aorbit").mean().abs().sort_values(by="dalt", ascending=False)
+dcps = df[["dorbit", "dalt"]].groupby("dorbit").mean().abs().sort_values(by="dalt", ascending=False)
 
-print(df[["dorbit", "dalt"]].groupby("dorbit").mean().abs().sort_values(by="dalt", ascending=True).head())
+# TODO: 画出质量最差的轨道
+worst_afile = acps.index[0]
+temp = df[df["aorbit"] == worst_afile].sort_values(by="dtime")
+# print(worst_afile, acps[worst_afile])
+plt.scatter(temp["dtime"], temp["dalt"])
+plt.savefig(f"figs/worst_orbit.png")
+print(temp["dalt"].mean())
+
 # 输出质量最差的几条轨道
 
 # TODO: 筛选最大的几个输出到文件
